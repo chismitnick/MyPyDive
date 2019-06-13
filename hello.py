1e540c795edbbe378cd917e24294479d80e6d0c9
csamiselo <chisambwe.samiselo@gmail.com>
2019-06-13 12:56:21 +0200

    Simple program usin print and input functions

---
+++ hello.py
@@ -0,0 +1,16 @@
+#A simple progran to display print() and input methods 
+
+
+print("Enter your first name:")
+first_name = input()
+
+print("Enter your last  name:")
+last_name = input()
+
+print("Enter your address:")
+
+address = input()
+
+print("Enter your Job Title")
+job_title = input()
+
