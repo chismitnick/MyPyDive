98a8e6fbd90b1802edba9f196012b41b6935c926
csamiselo <csamiselo@github.com>
2019-10-15 11:44:13 +0200

    Modifications plus addition of more programs

---
+++ simplemath2.py
@@ -0,0 +1,12 @@
+#Writen by csamiselo 15.10.2019
+
+print("This is how i count my livestock")
+print("Goats",12 + 30 + 60)
+
+print("Cows", 13 + 15 + 10)
+
+print ("Layers" ,1000 + 250 + 503 )
+
+print ("Are the layers more than the goats")
+print (12 + 30 + 60 < 1000 + 250 +503 )
+
---
+++ varsandnames.py
@@ -0,0 +1,17 @@
+#Written by csamiselo 15.10.2019
+#Simple python program to demonstrate how to use variables and name in python
+
+shirts  = 100 
+trousers = 200
+boxer_shorts  = 100
+socks = 60
+shoes = 250
+
+
+print("This is my clothing list")
+
+print("The number of shirts is:", shirts)
+print("The number of trousers is:", trousers)
+print("The number of Boxers is :",boxer_shorts)
+print("The number of socks is :",socks)
+print("The number of shoes is :", shoes)
