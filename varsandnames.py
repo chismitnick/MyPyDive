3d6bcde9be4eadaedc54dd876d38743266862867
csamiselo <chisambwe.samiselo@gmail.com>
2020-04-10 22:09:31 +0200

    Progress so far

--- varsandnames.py
+++ varsandnames.py
@@ -1,17 +1,15 @@
 # csamiselo@github.com 15.10.2019
-#Simple python program to demonstrate how to use variables and names in python
+# Simple python program to demonstrate how to use variables and names in python
 
-shirts  = 100 
+shirts = 100
 trousers = 200
-boxer_shorts  = 100
+boxer_shorts = 100
 socks = 60
 shoes = 250
 
 
-print("This is my clothing list")
-
 print("The number of shirts is:", shirts)
 print("The number of trousers is:", trousers)
-print("The number of Boxers is :",boxer_shorts)
-print("The number of socks is :",socks)
+print("The number of Boxers is :", boxer_shorts)
+print("The number of socks is :", socks)
 print("The number of shoes is :", shoes)
